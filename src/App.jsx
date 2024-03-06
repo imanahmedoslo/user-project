73c57064cf061e7a54bc2db457e5f6c740906df1
imanahmedoslo <imanahmedimanahmed@gmail.com>
2024-03-06 15:53:52 +0100

    upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import AddUser from './Components/Users/AddUser/AddUser'
+import UsersList from "./Components/Users/UserList/UsersList";
+
+
+function App() {
+
+    const [usersList, setUsersList] = useState([]);
+
+    const addUserHandler = (userName, userAge) => {
+        setUsersList((prevUsersList) => {
+            return [
+                ...prevUsersList,
+                { name: userName, age: userAge, id: Math.random.toString() }
+            ];
+        });
+    };
+
+
+    return (
+        <div>
+            <AddUser onAddUser={addUserHandler} />
+            <UsersList users={usersList} />
+        </div>
+    );
+}
+
+export default App;
